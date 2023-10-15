d45205f958d886f3ba81d406e41db0c179bc7383
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2023-10-15 15:21:21 +0200

    cration de tag pour la doc

--- exercice3.py
+++ exercice3.py
@@ -1,37 +1,28 @@
 from fastapi import FastAPI
-from fastapi import Header
-from pydantic import BaseModel
-from typing import Optional
 
-class Computer(BaseModel):
-    computerid: int
-    cpu: Optional[str]
-    gpu: Optional[str]
-    price: float
+api = FastAPI(openapi_tags=[
+    {
+        'name': 'home',
+        'description': 'default functions'
+    },
+    {
+        'name': 'items',
+        'description': 'functions that are used to deal with items'
+    }
+])
 
-api = FastAPI(
-    title="My API",
-    description="My own API powered by FastAPI.",
-    version="1.0.1")
-
-
-
-
-@api.get('/',name="Hello World")
+@api.get('/', tags=['home'])
 def get_index():
-    """Returns greetings
+    """returns greetings
     """
-    return {'greetings': 'welcome'}
-
-
-@api.put('/computer', name='Create a new computer')
-def get_computer(computer: Computer):
-    """Creates a new computer within the database
-    """
-    return computer
-
-@api.get('/custom', name='Get custom header')
-def get_content(custom_header: Optional[str] = Header(None, description='My own personal header')):
     return {
-        'Custom-Header': custom_header
+        'greetings': 'hello world'
+    }
+
+@api.get('/items', tags=['home', 'items'])
+def get_items():
+    """returns an item
+    """
+    return {
+        'item': "some item"
     }
